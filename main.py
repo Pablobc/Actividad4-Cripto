cf9650318513595ac7af59ebb11df7a64ecea2b9
Pablobc <31974681+Pablobc@users.noreply.github.com>
2021-10-12 02:19:09 -0300

    Función procesar

--- main.py
+++ main.py
@@ -1,39 +1,59 @@
 import random
 from datetime import datetime
+import struct
 
-entrada="Hola mundo"
-
+entrada="Hola MundoHola MundoHola MundoHola MundoHola Mundo"
 def cortadora(entrada):
 	string=entrada
-	salida=["","","","",""]
-	if len(entrada)<25:
-		while(len(string)<25):
+	salida=["","","","","","",""]
+	if len(entrada)<28:
+		while(len(string)<28):
 			string="0"+string
-		salida[0]=string[0:5]
-		salida[1]=string[5:10]
-		salida[2]=string[10:15]
-		salida[3]=string[15:20]
-		salida[4]=string[20:25]
-	elif len(entrada)>25:
-		string=entrada[0:25]
-		salida[0]=string[0:5]
-		salida[1]=string[5:10]
-		salida[2]=string[10:15]
-		salida[3]=string[15:20]
-		salida[4]=string[20:25]
+		salida[0]=bytes(string[0:4],encoding='utf-8')
+		salida[1]=bytes(string[4:8],encoding='utf-8')
+		salida[2]=bytes(string[8:12],encoding='utf-8')
+		salida[3]=bytes(string[12:16],encoding='utf-8')
+		salida[4]=bytes(string[16:20],encoding='utf-8')
+		salida[5]=bytes(string[20:24],encoding='utf-8')
+		salida[6]=bytes(string[24:28],encoding='utf-8')
+	elif len(entrada)>28:
+		string=entrada[0:28]
+		salida[0]=bytes(string[0:4],encoding='utf-8')
+		salida[1]=bytes(string[4:8],encoding='utf-8')
+		salida[2]=bytes(string[8:12],encoding='utf-8')
+		salida[3]=bytes(string[12:16],encoding='utf-8')
+		salida[4]=bytes(string[16:20],encoding='utf-8')
+		salida[5]=bytes(string[20:24],encoding='utf-8')
+		salida[6]=bytes(string[24:28],encoding='utf-8')
 	else:
-		salida[0]=string[0:5]
-		salida[1]=string[5:10]
-		salida[2]=string[10:15]
-		salida[3]=string[15:20]
-		salida[4]=string[20:25]
+		salida[0]=bytes(string[0:4],encoding='utf-8')
+		salida[1]=bytes(string[4:8],encoding='utf-8')
+		salida[2]=bytes(string[8:12],encoding='utf-8')
+		salida[3]=bytes(string[12:16],encoding='utf-8')
+		salida[4]=bytes(string[16:20],encoding='utf-8')
+		salida[5]=bytes(string[20:24],encoding='utf-8')
+		salida[6]=bytes(string[24:28],encoding='utf-8')
 	print(salida)
 	return(salida)
 
 def procesar(entrada):
-	salida=""
+	w=[0]*7
 	random.seed(datetime.now())
-	generador=random.randint(0,255)
-	print(generador)
+	generador=random.randint(0,31)
+	i=0
+	while(i<7):
+		w[i]=struct.unpack(b'>I', entrada[i])[0]
+		i+=1
+	print(w)
+	w[0]=(w[0]<<generador) & 0xffffffff
+	w[1]=(w[1]^generador) & 0xffffffff
+	w[2]=(w[2]>>generador) & 0xffffffff
+	w[3]=(w[3] & generador) & 0xffffffff
+	w[4]=(w[4] | generador) & 0xffffffff
+	w[5]=(w[5] ^ w[4] ^ generador) & 0xffffffff
+	w[6]=((w[6]^w[5]) << generador) & 0xffffffff
+	print(w)
+	return b''.join(struct.pack(b'>I', h) for h in w)
 	
-procesar("")
+
+print(procesar(cortadora(entrada)))
